d5e07efc6e96ee9edca7155ee1a21855c8643205
Bangkyunghwan <qkdrudghks0297@naver.com>
2022-02-16 15:17:17 +0900

    Add & feat: Add Main Menu Scene and creat Main Menu UI(only design)

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public void onClickNewGame()
+    {
+        Debug.Log("Make New Game");
+    }
+
+    public void onClickLoadGame()
+    {
+        Debug.Log("Load Game");
+        
+    }
+
+    public void onClickOptions()
+    {
+        Debug.Log("Options");
+
+    }
+
+    public void onClickExit()
+    {
+        Debug.Log("Exit");
+    }
+}
