a5c3ac910efc4c360148cae22b3241e9d035e4ff
Bangkyunghwan <qkdrudghks0297@naver.com>
2022-02-17 13:43:07 +0900

    Add: create 'Option' scene

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
 
 public class MainMenu : MonoBehaviour
 {
@@ -30,7 +33,7 @@ public class MainMenu : MonoBehaviour
     public void onClickOptions()
     {
         Debug.Log("Options");
-
+        SceneManager.LoadScene("Options");
     }
 
     public void onClickExit()
